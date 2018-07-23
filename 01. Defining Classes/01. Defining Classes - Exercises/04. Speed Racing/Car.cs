9bea196710f0651014efae1f8744cc22e2ce5af4
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-23 19:01:29 +0200

    Problem 7. Speed Racing

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/04. Speed Racing/Car.cs
@@ -0,0 +1,75 @@
+﻿namespace _04._Speed_Racing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionFor1Km)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionFor1km = fuelConsumptionFor1Km;
+            this.DistanceTraveled = 0;
+        }
+
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionFor1km;
+        private double distanceTraveled;
+        
+        public string Model
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public double FuelAmount
+        {
+            get => fuelAmount;
+            set => fuelAmount = value;
+        }
+
+        public double FuelConsumptionFor1km
+        {
+            get => fuelConsumptionFor1km;
+            private set => fuelConsumptionFor1km = value;
+        }
+
+        public double DistanceTraveled
+        {
+            get => distanceTraveled;
+            private set => distanceTraveled = value;
+        }
+
+        public static Car Parse(string input)
+        {
+            var tokens = input.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var carModel = tokens[0];
+            var carFuelAmount = double.Parse(tokens[1]);
+            var carFuelConsumption = double.Parse(tokens[2]);
+
+            return new Car(carModel, carFuelAmount, carFuelConsumption);
+        }
+
+        public bool Drive(double amountOfKm)
+        {
+            var currentFuel = this.FuelAmount;
+            var currentConsumption = this.FuelConsumptionFor1km;
+            var neededFuel = currentConsumption * amountOfKm;
+
+            if (currentFuel < neededFuel)
+            {
+                return false;
+            }
+            else
+            {
+                this.fuelAmount -= neededFuel;
+                this.DistanceTraveled += amountOfKm;
+                return true;
+            }
+        }
+    }
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/04. Speed Racing/SpeedRacing.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace _04._Speed_Racing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SpeedRacing
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var cars = new List<Car>();
+
+            for (var i = 0; i < n; i++)
+            {
+                cars.Add(Car.Parse(Console.ReadLine()));
+            }
+
+            var inputLine = string.Empty;
+
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                var tokens = inputLine.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                var model = tokens[1];
+                var amountOfKm = double.Parse(tokens[2]);
+
+                var currentCar = cars.Where(x => x.Model == model).First();
+
+                if (!currentCar.Drive(amountOfKm))
+                {
+                    Console.WriteLine($"Insufficient fuel for the drive");
+                }
+            }
+
+            cars.ForEach(x => Console.WriteLine($"{x.Model} {x.FuelAmount:F2} {x.DistanceTraveled}"));
+        }
+    }
+}
