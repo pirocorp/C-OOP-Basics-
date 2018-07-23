2d191e35ccbab419c4f66f7ec9120cdf1fd54e6d
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-23 15:08:30 +0200

    Problem 4. Opinion Poll

--- 01. Defining Classes/01. Defining Classes - Exercises/01. Define a Class Person/Program.cs
+++ 01. Defining Classes/01. Defining Classes - Exercises/01. Define a Class Person/Program.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
+    {
+        //OldestFamilyMember();
+        OpinionPoll();
+    }
+
+    private static void OpinionPoll()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        var listOfPeople = new List<Person>();
+
+        for (var i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var name = tokens[0];
+            var age = int.Parse(tokens[1]);
+
+            var currentPerson = new Person(name, age);
+
+            listOfPeople.Add(currentPerson);
+        }
+
+        listOfPeople
+            .Where(x => x.Age > 30)
+            .OrderBy(x => x.Name)
+            .ToList()
+            .ForEach(x => Console.WriteLine($"{x.Name} - {x.Age}"));
+    }
+
+    private static void OldestFamilyMember()
     {
         var n = int.Parse(Console.ReadLine());
 
