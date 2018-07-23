36db62e3ef8e9b0f08352a7d22c583e0e368406b
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-23 18:13:16 +0200

    Problem 6. Company Roster

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/03. Company Roster/CompanyRoster.cs
@@ -0,0 +1,38 @@
+﻿namespace _03._Company_Roster
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CompanyRoster
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (var i = 0; i < n; i++)
+            {
+                employees.Add(Employee.Parse(Console.ReadLine()));
+            }
+
+            var highAverageDepartment = employees
+                .GroupBy(x => x.Department, (s, enumerable) => new
+                {
+                    department = s,
+                    workers = employees.Where(e => e.Department == s).ToList(),
+                    average = employees.Where(e => e.Department == s).Select(e => e.Salary).Average(),
+                })
+                .OrderByDescending(g => g.average)
+                .First();
+
+            Console.WriteLine($"Highest Average Salary: {highAverageDepartment.department}");
+
+            highAverageDepartment.workers
+                .OrderByDescending(x => x.Salary)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x.Name} {x.Salary:F2} {x.Email} {x.Age}"));
+        }
+    }
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/03. Company Roster/Employee.cs
@@ -0,0 +1,107 @@
+﻿namespace _03._Company_Roster
+{
+    using System;
+
+    public class Employee
+    {
+        public Employee(string name, decimal salary, string position, string department)
+        : this(name, salary, position, department, "n/a")
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email)
+            : this(name, salary, position, department, email, -1)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email, int age)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Department = department;
+            Email = email;
+            Age = age;
+        }
+
+        private string name;
+        private decimal salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public decimal Salary
+        {
+            get => salary;
+            set => salary = value;
+        }
+
+        public string Position
+        {
+            get => position;
+            set => position = value;
+        }
+
+        public string Department
+        {
+            get => department;
+            set => department = value;
+        }
+
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public static Employee Parse(string input)
+        {
+            var email = "n/a";
+            var age = -1;
+            var personData = input
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (personData.Length > 4)
+            {
+                int parsed;
+                var isdigit = int.TryParse(personData[4], out parsed);
+
+                if (isdigit)
+                {
+                    age = parsed;
+                }
+                else
+                {
+                    email = personData[4];
+                }
+
+                if (personData.Length > 5)
+                {
+                    if (isdigit)
+                    {
+                        email = personData[5];
+                    }
+                    else
+                    {
+                        age = int.Parse(personData[5]);
+                    }
+                }
+            }
+
+            return new Employee(personData[0], decimal.Parse(personData[1]), personData[2], personData[3], email, age);
+        }
+    }
+}
