64350f93832fe4032ac6204e26410424187bf10a
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-23 15:35:35 +0200

    Problem 5. Date Modifier

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/02. Date Modifier/DateModifier.cs
@@ -0,0 +1,53 @@
+﻿namespace _02._Date_Modifier
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DateModifier
+    {
+        public DateModifier(string firstDate, string secondDate)
+        {
+            this.firstDate = firstDate;
+            this.secondDate = secondDate;
+        }
+
+        private string firstDate;
+        private string secondDate;
+
+        public string FirstDate
+        {
+            get => firstDate;
+            set => firstDate = value;
+        }
+
+        public string SecondDate
+        {
+            get => secondDate;
+            set => secondDate = value;
+        }
+
+        public int DaysBetweenDates()
+        {
+            var tokens = this.FirstDate
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var dateYears = int.Parse(tokens[0]);
+            var dateMonts = int.Parse(tokens[1]);
+            var dateDays = int.Parse(tokens[2]);
+
+            var currentFirstDate = new DateTime(dateYears, dateMonts, dateDays);
+
+            tokens = this.SecondDate
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            dateYears = int.Parse(tokens[0]);
+            dateMonts = int.Parse(tokens[1]);
+            dateDays = int.Parse(tokens[2]);
+
+            var currentSeconDate = new DateTime(dateYears, dateMonts, dateDays);
+
+            return (int)Math.Floor(Math.Abs((currentFirstDate - currentSeconDate).TotalDays));
+        }
+    }   
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/02. Date Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02._Date_Modifier
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var x = new DateModifier(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(x.DaysBetweenDates());
+        }
+    }
+}
