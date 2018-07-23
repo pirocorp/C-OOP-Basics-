e0313dd36ba26def6d2a05c8a34e48243b03566f
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-24 00:13:53 +0200

    Problem 9. Rectangle Intersection

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/06. Rectangle Intersection/Rectangle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _06._Rectangle_Intersection
+{
+    public class Rectangle
+    {
+        public Rectangle(string id, double width, double height, double topX, double topY)
+        {
+            this.id = id;
+            this.width = width;
+            this.height = height;
+            this.topX = topX;
+            this.topY = topY;
+        }
+
+        private string id;
+        private double width;
+        private double height;
+        private double topX;
+        private double topY;
+        
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public double Width
+        {
+            get => width;
+            set => width = value;
+        }
+
+        public double Height
+        {
+            get => height;
+            set => height = value;
+        }
+
+        public double TopX
+        {
+            get => topX;
+            set => topX = value;
+        }
+
+        public double TopY
+        {
+            get => topY;
+            set => topY = value;
+        }
+
+        public static Rectangle Parse(string input)
+        {
+            var tokens = input.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var id = tokens[0];
+            var width = double.Parse(tokens[1]);
+            var height = double.Parse(tokens[2]);
+            var topX = double.Parse(tokens[3]);
+            var topY = double.Parse(tokens[4]);
+
+            return new Rectangle(id, width, height, topX, topY);
+        }
+
+        public bool Intersect(Rectangle inputRectangle)
+        {
+            var firstRectangleTopX = this.TopX;
+            var firstRectangleTopY = this.TopY;
+            var firstRectangleBottomX = this.topX + this.width;
+            var firstRectangleBottomY = this.topY + this.height;
+
+            var firstMinX = Math.Min(firstRectangleTopX, firstRectangleBottomX);
+            var firstMaxX = Math.Max(firstRectangleTopX, firstRectangleBottomX);
+            var firstMinY = Math.Min(firstRectangleTopY, firstRectangleBottomY);
+            var firstMaxY = Math.Max(firstRectangleTopY, firstRectangleBottomY);
+
+            var secondRectangleTopX = inputRectangle.TopX;
+            var secondRectangleTopY = inputRectangle.TopY;
+            var secondRectangleBottomX = inputRectangle.TopX + inputRectangle.width;
+            var secondRectangleBottomY = inputRectangle.TopY + inputRectangle.height;
+
+            var secondMinX = Math.Min(secondRectangleTopX, secondRectangleBottomX);
+            var secondMaxX = Math.Max(secondRectangleTopX, secondRectangleBottomX);
+            var secondMinY = Math.Min(secondRectangleTopY, secondRectangleBottomY);
+            var secondMaxY = Math.Max(secondRectangleTopY, secondRectangleBottomY);
+
+            var noOverlap = firstMinX > secondMaxX || secondMinX > firstMaxX ||
+                            firstMinY > secondMaxY || secondMinY > firstMaxY;
+
+            return !noOverlap;
+        }
+    }
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/06. Rectangle Intersection/RectangleIntersection.cs
@@ -0,0 +1,46 @@
+﻿namespace _06._Rectangle_Intersection
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class RectangleIntersection
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine().Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rectangles = new Dictionary<string, Rectangle>();
+
+            var n = tokens[0];
+            var m = tokens[1];
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+
+                var rectangleId = inputLine
+                    .Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries)
+                    .First();
+
+                var currentRectangle = Rectangle.Parse(inputLine);
+
+                rectangles[rectangleId] = currentRectangle;
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                var rectangleIds = Console.ReadLine().Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                var firstId = rectangleIds[0];
+                var secondId = rectangleIds[1];
+
+                var firstRectangle = rectangles[firstId];
+                var secondRectangle = rectangles[secondId];
+
+                Console.WriteLine(firstRectangle.Intersect(secondRectangle).ToString().ToLower());
+            }
+        }
+    }
+}
