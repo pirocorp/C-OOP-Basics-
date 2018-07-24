5adb4f99f4af2f073af206d9bb69cc372ee64a9d
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-24 19:48:26 +0200

    Problem 14. *Cat Lady

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/11. Cat Lady/Cat.cs
@@ -0,0 +1,59 @@
+﻿namespace _11._Cat_Lady
+{
+    using System;
+
+    public class Cat
+    {
+        public Cat(string breed, string name, decimal specificCharacteristic)
+        {
+            this.breed = breed;
+            this.name = name;
+            this.specificCharacteristic = specificCharacteristic;
+        }
+
+        private string breed;
+        private string name;
+        private decimal specificCharacteristic;
+        
+        public string Breed
+        {
+            get => breed;
+            set => breed = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public decimal SpecificCharacteristic
+        {
+            get => specificCharacteristic;
+            set => specificCharacteristic = value;
+        }
+
+        public static Cat Parse(string inputLine)
+        {
+            var tokens = inputLine.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            return Parse(tokens);
+        }
+
+        public static Cat Parse(string[] input)
+        {
+            var breed = input[0];
+            var name = input[1];
+            var specificCharacteristic = decimal.Parse(input[2]);
+
+            return new Cat(breed, name, specificCharacteristic);
+        }
+
+        public override string ToString()
+        {
+            var specificCharacteristicString =
+                breed == "Cymric" ? $"{this.specificCharacteristic:F2}" : $"{this.specificCharacteristic}";
+            return $"{this.breed} {this.name} {specificCharacteristicString}";
+        }
+    }
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/11. Cat Lady/CatLady.cs
@@ -0,0 +1,26 @@
+﻿namespace _11._Cat_Lady
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public class CatLady
+    {
+        public static void Main(string[] args)
+        {
+            var inputLine = string.Empty;
+
+            var cats = new Dictionary<string, Cat>();
+
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                var tokens = inputLine.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var name = tokens[1];
+
+                cats.Add(name, Cat.Parse(tokens));
+            }
+
+            var searchedCat = Console.ReadLine();
+            Console.WriteLine(cats[searchedCat]);
+        }
+    }
+}
