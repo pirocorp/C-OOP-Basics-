ab8f2e9c317cdd92ba648030f8ae35c722233f73
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-25 00:02:01 +0200

    Drawing tool

---
+++ 01. Defining Classes/01. Defining Classes - Exercises/12. Drawing Tool/DrawingTool.cs
@@ -0,0 +1,40 @@
+﻿namespace _12._Drawing_Tool
+{
+    using System.Text;
+
+    public class DrawingTool
+    {
+        public DrawingTool(int width, int height, string name)
+        {
+            this.width = width;
+            this.height = height;
+            this.name = name;
+        }
+
+        private int width;
+        private int height;
+        private string name;
+
+        public string Draw()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("|");
+            sb.Append(new string('-', width));
+            sb.AppendLine("|");
+
+            for (var i = 0; i < height - 2; i++)
+            {
+                sb.Append("|");
+                sb.Append(new string(' ', width));
+                sb.AppendLine("|");
+            }
+
+            sb.Append("|");
+            sb.Append(new string('-', width));
+            sb.AppendLine("|");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 01. Defining Classes/01. Defining Classes - Exercises/12. Drawing Tool/DrawingToolProgram.cs
@@ -0,0 +1,27 @@
+﻿namespace _12._Drawing_Tool
+{
+    using System;
+
+    public class DrawingToolProgram
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+
+            var width = int.Parse(Console.ReadLine());
+            var height = 0;
+
+            if (name == "Square")
+            {
+                height = width;
+            }
+            else
+            {
+                height = int.Parse(Console.ReadLine());
+            }
+
+            var figure = new DrawingTool(width, height, name);
+            Console.WriteLine(figure.Draw());
+        }
+    }
+}
