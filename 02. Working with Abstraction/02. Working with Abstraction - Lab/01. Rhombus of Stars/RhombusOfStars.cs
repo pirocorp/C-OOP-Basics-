7a47ae8e3ef82216533c9f9d4e54c231dc9d5275
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-26 16:40:00 +0200

    Problem 1. Rhombus of Stars

---
+++ 02. Working with Abstraction/02. Working with Abstraction - Lab/01. Rhombus of Stars/RhombusOfStars.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Rhombus_of_Stars
+{
+    using System;
+
+    public class RhombusOfStars
+    {
+        public static void Main()
+        {
+            var rombusSize = int.Parse(Console.ReadLine());
+
+            for (var stars = 1; stars <= rombusSize; stars++)
+            {
+                PrintLine(rombusSize, stars, Console.WriteLine);
+            }
+
+            for (var stars = rombusSize - 1; stars > 0; stars--)
+            {
+                PrintLine(rombusSize, stars, Console.WriteLine);
+            }
+        }
+
+        private static void PrintLine(int rombusSize, int starsCount, Action<string> printOutput)
+        {
+            var whitespacesCount = rombusSize - starsCount;
+            var stars = new string('*', starsCount).ToCharArray();
+            var line = $"{new string(' ', whitespacesCount)}{string.Join(" ", stars)}";
+            printOutput(line);
+        }
+    }
+}
