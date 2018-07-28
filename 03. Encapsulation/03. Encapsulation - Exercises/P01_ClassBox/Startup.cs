6e09e27ac10baa5f2b5fa4f9f200a253f50a2edc
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-28 15:15:23 +0200

    Problem 2. Class Box Data Validation

--- 03. Encapsulation/03. Encapsulation - Exercises/P01_ClassBox/Startup.cs
+++ 03. Encapsulation/03. Encapsulation - Exercises/P01_ClassBox/Startup.cs
@@ -14,14 +14,14 @@ public class Startup
         try
         {
             box = new Box(length, width, height);
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
         }
         catch (ArgumentException e)
         {
             Console.WriteLine(e.Message);
         }
-
-        Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
-        Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
-        Console.WriteLine($"Volume - {box.Volume():F2}");
     }
 }
