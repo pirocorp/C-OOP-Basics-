93c04d05def69c0c90f2ba2a6fb3aba600cad19e
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-28 12:56:03 +0200

    1. Sort Persons by Name and Age

---
+++ 03. Encapsulation/03. Encapsulation - Lab/P01_Persons/Person.cs
@@ -0,0 +1,21 @@
+﻿    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName => this.firstName;
+        public int Age => this.age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.lastName} is {this.age} years old.";
+        }
+    }
