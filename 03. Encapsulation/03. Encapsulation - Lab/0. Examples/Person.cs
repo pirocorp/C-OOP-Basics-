6e18a8183fa1c33bd0dfca5be75fa2ff8c9c38d5
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-28 12:28:26 +0200

    0. Examples

---
+++ 03. Encapsulation/03. Encapsulation - Lab/0. Examples/Person.cs
@@ -0,0 +1,106 @@
+﻿namespace _0._Examples
+{
+    using System;
+
+    public class Person
+    {
+        private const int NameMinLenght = 3;
+        private const decimal MinSalary = 460M;
+
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+        private decimal _salary;
+
+        private string FirstName
+        {
+            get => _firstName;
+
+            set
+            {
+                ValidateFieldLenght(value, nameof(FirstName));
+                _firstName = value;
+            }
+        }
+
+        private string LastName
+        {
+            get => _lastName;
+
+            set
+            {
+                ValidateFieldLenght(value, nameof(LastName));
+                _lastName = value;
+            }
+        }
+
+        public string Name => $"{FirstName} {LastName}";
+
+        public int Age
+        {
+            get => _age;
+
+            private set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentException($"{nameof(Age)} cannot be less than 0 or more than 120");
+                }
+
+                _age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get => _salary;
+
+            private set
+            {
+                if (value < MinSalary)
+                {
+                    throw new ArgumentException($"{nameof(Salary)} cannot be less than {MinSalary:F2}");
+                }
+
+                _salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary = 0)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        private static void ValidateFieldLenght(string fieldValue, string fieldName)
+        {
+            if (fieldValue == null)
+            {
+                throw new ArgumentNullException($"{fieldName}");
+            }
+
+            if (fieldValue.Length < NameMinLenght)
+            {
+                throw new ArgumentException($"{fieldName} cannot be less than {NameMinLenght} symbols");
+            }
+        }
+
+        public void IncreaseSalary(int percent)
+        {
+            if (percent < 0)
+            {
+                throw new ArgumentException($"{nameof(percent)} cannot be negative");
+            }
+
+            var ratio = (100 + percent) / 100M;
+            _salary *= ratio;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} - {Salary:F2}";
+        }
+    }
+}
---
+++ 03. Encapsulation/03. Encapsulation - Lab/0. Examples/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace _0._Examples
+{
+    using System;
+
+    public class Startup    
+    {
+        public static void Main()
+        {
+            try
+            {
+                var person = new Person("Stancho", "Stamatov", 23, 556);
+                Console.WriteLine(person.Name);
+                Console.WriteLine(person);
+
+                person.IncreaseSalary(15);
+                Console.WriteLine(person);
+
+            }
+            catch (Exception ex) when(ex is ArgumentException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
