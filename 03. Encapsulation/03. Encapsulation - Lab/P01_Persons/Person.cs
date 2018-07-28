820cbcfd7116c3274bb1c3484a7b65971870e901
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-28 13:08:52 +0200

    2. Salary Increase

--- 03. Encapsulation/03. Encapsulation - Lab/P01_Persons/Person.cs
+++ 03. Encapsulation/03. Encapsulation - Lab/P01_Persons/Person.cs
@@ -3,19 +3,34 @@
         private string firstName;
         private string lastName;
         private int age;
+        private decimal salary;
 
         public string FirstName => this.firstName;
         public int Age => this.age;
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.firstName = firstName;
             this.lastName = lastName;
             this.age = age;
+            this.salary = salary;
         }
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this.lastName} is {this.age} years old.";
+            return $"{this.FirstName} {this.lastName} receives {this.salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            var ratio = percentage / 100;
+            var rise = this.salary * ratio;
+
+            if (this.Age < 30)
+            {
+                rise = rise / 2;
+            }
+
+            this.salary += rise;
         }
     }
--- 03. Encapsulation/03. Encapsulation - Lab/P01_Persons/Startup.cs
+++ 03. Encapsulation/03. Encapsulation - Lab/P01_Persons/Startup.cs
@@ -8,16 +8,20 @@
         {
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
-            for (int i = 0; i < lines; i++)
+
+            for (var i = 0; i < lines; i++)
             {
                 var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    decimal.Parse(cmdArgs[3]));
+
                 persons.Add(person);
             }
 
-            persons.OrderBy(p => p.FirstName)
-                .ThenBy(p => p.Age)
-                .ToList()
-                .ForEach(p => Console.WriteLine(p.ToString()));
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
         }
     }
