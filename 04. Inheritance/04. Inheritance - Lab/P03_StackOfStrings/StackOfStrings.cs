91f8161267ae77df2cfd93a0535f475d0c711720
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-30 17:21:36 +0200

    1.	Stack of Strings

---
+++ 04. Inheritance/04. Inheritance - Lab/P03_StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public bool IsEmpty()
+    {
+        return data.Count == 0;
+    }
+
+    public void Push(string newItem)
+    {
+        data.Add(newItem);
+    }
+
+    public string Peek()
+    {
+        var result = string.Empty;
+
+        if (!IsEmpty())
+        {
+            var lastElementIndex = data.Count - 1;
+            result = data[lastElementIndex];
+        }
+
+        return result;
+    }
+
+    public string Pop()
+    {
+        var result = string.Empty;
+
+        if (!IsEmpty())
+        {
+            result = Peek();
+            var lastElementIndex = data.Count - 1;
+            data.RemoveAt(lastElementIndex);
+        }
+
+        return result;
+    }
+}
---
+++ 04. Inheritance/04. Inheritance - Lab/P03_StackOfStrings/Startup.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Startup
+{
+    static void Main()
+    {
+
+    }
+}
