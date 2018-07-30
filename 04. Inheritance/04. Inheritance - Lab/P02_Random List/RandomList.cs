cdde4cf735fa3d5a5a3492c0c9729043df4db051
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-30 17:03:12 +0200

    4. Random List

---
+++ 04. Inheritance/04. Inheritance - Lab/P02_Random List/RandomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RandomList : List<string>
+{
+    private Random random;
+
+    public RandomList()
+    {
+        this.random = new Random();
+    }
+
+    public string RandomString()
+    {
+        string result = null;
+
+        if (this.Count > 0)
+        {
+            var randomIndex = random.Next(0, this.Count);
+            result = this[randomIndex];
+            this.RemoveAt(randomIndex);
+        }
+
+        return result;
+    }
+}
---
+++ 04. Inheritance/04. Inheritance - Lab/P02_Random List/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Startup
+{
+    static void Main()
+    {
+        var randomList = new RandomList();
+        randomList.Add("34wfsdsd");
+        randomList.Add("444");
+        randomList.Add("asdf");
+        randomList.Add("55");
+
+        Console.WriteLine(randomList.RandomString());
+    }
+}
