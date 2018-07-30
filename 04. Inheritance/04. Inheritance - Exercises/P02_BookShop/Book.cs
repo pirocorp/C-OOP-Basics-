923b3386da3c65dbd46b5bd493fe071d8a801494
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-30 19:59:08 +0200

    Problem 2. Book Shop

---
+++ 04. Inheritance/04. Inheritance - Exercises/P02_BookShop/Book.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+public class Book
+{
+    private string title;
+    private string author;
+    private decimal price;
+
+    public Book(string author, string title, decimal price)
+    {
+        this.Author = author;
+        this.Title = title;
+        this.Price = price;
+    }
+
+    public string Title
+    {
+        get => this.title;
+        protected set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException($"{nameof(Title)} not valid!");
+            }
+
+            this.title = value;
+        }
+    }
+
+    public string Author
+    {
+        get => this.author;
+        protected set
+        {
+            var names = value.Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (names.Length == 2 && char.IsDigit(names[1][0]))
+            {
+                throw new ArgumentException($"{nameof(Author)} not valid!");
+            }
+
+            this.author = value;
+        }
+    }
+
+    public virtual decimal Price
+    {
+        get => this.price;
+        protected set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(Price)} not valid!");
+            }
+
+            this.price = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        var resultBuilder = new StringBuilder();
+        resultBuilder.AppendLine($"Type: {this.GetType().Name}")
+            .AppendLine($"Title: {this.Title}")
+            .AppendLine($"Author: {this.Author}")
+            .AppendLine($"Price: {this.Price:F2}");
+
+        var result = resultBuilder.ToString().TrimEnd();
+        return result;
+    }
+}
---
+++ 04. Inheritance/04. Inheritance - Exercises/P02_BookShop/GoldenEditionBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class GoldenEditionBook : Book
+{
+    private const decimal PRICE_INCREASE_MULTYPLAYER = 1.3M;
+
+    public GoldenEditionBook(string author, string title, decimal price)
+        : base(author, title, price)
+    {
+    }
+
+    public override decimal Price => base.Price * PRICE_INCREASE_MULTYPLAYER;
+};
