8c41ba0ef5258e6291dc1c36eb16aaef27c5763f
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-30 16:22:40 +0200

    2. Multiple Inheritance

--- 04. Inheritance/04. Inheritance - Lab/P01_Inheritance/Startup.cs
+++ 04. Inheritance/04. Inheritance - Lab/P01_Inheritance/Startup.cs
@@ -4,8 +4,9 @@ public class Startup
 {
     static void Main()
     {
-        var dog = new Dog();
-        dog.Eat();
-        dog.Bark();
+        var puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
     }
 }
