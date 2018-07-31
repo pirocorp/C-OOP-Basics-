4f096c547b7560d44a9ad4dca8c47c9b49a20879
Zdravko Zdravkov <pirocorp@abv.bg>
2018-07-31 18:24:25 +0200

    Problem 1.	Animals

---
+++ 04. Inheritance/04. Inheritance - Exercises/P06_Animals/Animal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Animal : ISoundProducable
+{
+    private string name;
+    private int age;
+    private string gender;
+
+    public Animal(string name, int age, string gender)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Gender = gender;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set
+        {
+            ValidateStrings(value, nameof(Name));
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get => this.age;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException($"Invalid input!");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public string Gender
+    {
+        get => this.gender;
+        set
+        {
+            ValidateStrings(value, nameof(Gender));
+            this.gender = value;
+        }
+    }
+
+    private static void ValidateStrings(string value, string nameOfProperty)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException($"Invalid input!");
+        }
+    }
+
+    public virtual string ProduceSound()
+    {
+        return $"Animal";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().Name}" + Environment.NewLine +
+               $"{this.Name} {this.Age} {this.Gender}" + Environment.NewLine +
+               $"{ProduceSound()}" + Environment.NewLine;
+    }
+}
---
+++ 04. Inheritance/04. Inheritance - Exercises/P06_Animals/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Startup
+{
+    public static void Main()
+    {
+        var animals = new List<Animal>();
+
+        var animalType = string.Empty;
+
+        while ((animalType = Console.ReadLine()) != "Beast!")
+        {
+            try
+            {
+                ReadAndCreateAnimal(animalType, animals);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        animals.ForEach(Console.Write);
+    }
+
+    private static void ReadAndCreateAnimal(string animalType, List<Animal> animals)
+    {
+        var tokens = Console.ReadLine().Split();
+
+        var name = tokens[0];
+        var age = int.Parse(tokens[1]);
+        string gender = null;
+
+        if (tokens.Length == 3)
+        {
+            gender = tokens[2];
+        }
+
+        switch (animalType)
+        {
+            case "Cat":
+                var cat = new Cat(name, age, gender);
+                animals.Add(cat);
+                break;
+            case "Dog":
+                var dog = new Dog(name, age, gender);
+                animals.Add(dog);
+                break;
+            case "Frog":
+                var frog = new Frog(name, age, gender);
+                animals.Add(frog);
+                break;
+            case "Tomcat":
+                var tomcat = new Tomcat(name, age);
+                animals.Add(tomcat);
+                break;
+            case "Kitten":
+                var kitten = new Kitten(name, age);
+                animals.Add(kitten);
+                break;
+            default:
+                throw new ArgumentException("Invalid input!");
+        }
+    }
+}
