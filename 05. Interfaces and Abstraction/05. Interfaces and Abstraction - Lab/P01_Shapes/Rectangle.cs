699be318d9691ae241569f16788b3eb0bfbe0801
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-01 16:41:26 +0200

    1. Shapes

---
+++ 05. Interfaces and Abstraction/05. Interfaces and Abstraction - Lab/P01_Shapes/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Rectangle : IDrawable
+{
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public int Width
+    {
+        get => this.width;
+        private set
+        {
+            Validator.ValidateInt(value, nameof(Width));
+            this.width = value;
+        }
+    }
+
+    public int Height
+    {
+        get => this.height;
+        private set
+        {
+            Validator.ValidateInt(value, nameof(Height));
+            this.height = value;
+        }
+    }
+
+    public void Draw()
+    {
+        DrawLine(this.Width, '*', '*');
+
+        for (var line = 1; line < this.Height - 1; line++)
+        {
+            DrawLine(this.Width, ' ', '*');
+        }
+
+        DrawLine(this.Width, '*', '*');
+    }
+
+    private void DrawLine(int width, char mid, char end)
+    {
+        Console.Write(end);
+
+        for (var col = 0; col < width - 1; col++)
+        {
+            Console.Write(mid);
+        }
+
+        Console.WriteLine(end);
+    }
+}
---
+++ 05. Interfaces and Abstraction/05. Interfaces and Abstraction - Lab/P01_Shapes/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class Validator
+{
+    public static void ValidateInt(int value, string nameOfProperty)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException($"{nameOfProperty} cannot be negative or zero");
+        }
+    }
+}
