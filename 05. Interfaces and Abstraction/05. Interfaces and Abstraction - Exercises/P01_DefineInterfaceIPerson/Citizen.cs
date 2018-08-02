de9a6f4dad8016b76db7cb1e1c5cc31287380409
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-02 11:29:54 +0200

    Problem 1. Define an Interface IPerson

---
+++ 05. Interfaces and Abstraction/05. Interfaces and Abstraction - Exercises/P01_DefineInterfaceIPerson/Citizen.cs
@@ -0,0 +1,31 @@
+﻿public class Citizen : IPerson
+{
+    private string name;
+    private int age;
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set
+        {
+            Validator.ValidateString(value, nameof(Name));
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get => this.age;
+        set
+        {
+            Validator.ValidateAge(value, nameof(Age));
+            this.age = value;
+        }
+    }
+}
---
+++ 05. Interfaces and Abstraction/05. Interfaces and Abstraction - Exercises/P01_DefineInterfaceIPerson/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Validator
+{
+    public static void ValidateString(string value, string nameOfProperty)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException($"{nameOfProperty} cannot be null, empty or white space");
+        }
+    }
+
+    public static void ValidateAge(int value, string nameOfProperty)
+    {
+        if (value < 0 || value > 200)
+        {
+            throw new ArgumentException($"Age cannot be negative or more than 200");
+        }
+    }
+}
