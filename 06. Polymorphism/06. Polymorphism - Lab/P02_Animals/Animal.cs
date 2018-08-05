162ceea9e62f1c4b3c0423c358779624dafef2b3
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-05 13:49:44 +0200

    02. Animals

---
+++ 06. Polymorphism/06. Polymorphism - Lab/P02_Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿public abstract class Animal
+{
+    private string name;
+    private string favouriteFood;
+
+    protected Animal(string name, string favouriteFood)
+    {
+        this.Name = name;
+        this.FavouriteFood = favouriteFood;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set => this.name = value;
+    }
+
+    public string FavouriteFood
+    {
+        get => this.favouriteFood;
+        set => this.favouriteFood = value;
+    }
+
+    public virtual string ExplainSelf()
+    {
+        return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+    }
+}
