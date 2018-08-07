e6be32be1e436d818d0aa73ef1279a66cdb13bd2
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-07 17:07:19 +0200

    Problem 2. Distance Calculator

---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P02_DistanceCalculator/Point3D.cs
@@ -0,0 +1,50 @@
+﻿namespace P02_DistanceCalculator
+{
+    using System;
+
+    public class Point3D
+    {
+        private static readonly Point3D StartingPoint;
+
+        static Point3D()
+        {
+            StartingPoint = new Point3D();
+        }
+
+        public Point3D()
+        {
+            this.X = 0;
+            this.Y = 0;
+            this.Z = 0;
+        }
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public static Point3D GetStartingPoint => StartingPoint;
+
+        public double DistanceToPoint(Point3D endPoint)
+        {
+            var distance = Math.Pow((endPoint.X - this.X), 2) + 
+                           Math.Pow((endPoint.Y - this.Y), 2) +
+                           Math.Pow((endPoint.Z - this.Z), 2);
+
+            return Math.Sqrt(distance);
+        }
+
+        public override string ToString()
+        {
+            return $"{{{this.X}, {this.Y}, {this.Z}}}";
+        }
+    }
+}
---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P02_DistanceCalculator/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace P02_DistanceCalculator
+{
+    using System;
+    using P02_DistanceCalculator;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var point1 = new Point3D(1, 1, 0);
+            var point2 = new Point3D(2, 1, 2);
+
+            Console.WriteLine(point1.DistanceToPoint(point2));
+            Console.WriteLine(point2.DistanceToPoint(point1));
+        }
+    }
+}
