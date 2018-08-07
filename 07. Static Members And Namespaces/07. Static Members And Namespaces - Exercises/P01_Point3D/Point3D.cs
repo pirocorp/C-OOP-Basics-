d5d34c6f9c9981b2c8c76581c6d1299ee44bd745
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-07 17:06:44 +0200

    Problem 1. Point3D

---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P01_Point3D/Point3D.cs
@@ -0,0 +1,39 @@
+﻿namespace P01_Point3D
+{
+    public class Point3D
+    {
+        private static readonly Point3D StartingPoint;
+
+        static Point3D()
+        {
+            StartingPoint = new Point3D();
+        }
+
+        public Point3D()
+        {
+            this.X = 0;
+            this.Y = 0;
+            this.Z = 0;
+        }
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public static Point3D GetStartingPoint => StartingPoint;
+
+        public override string ToString()
+        {
+            return $"{{{this.X}, {this.Y}, {this.Z}}}";
+        }
+    }
+}
---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P01_Point3D/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_Point3D
+{
+    using System;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            var p1 = new Point3D(3, 4, 5);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(Point3D.GetStartingPoint);
+        }
+    }
+}
