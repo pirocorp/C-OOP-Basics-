4f27fa56345610feefaa29069eecf179c30a0385
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-08 19:52:49 +0200

    Problem 1. Square Root

---
+++ 08. Exception Handling/08. Exception Handling - Exercises/P01_SquareRoot/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace P01_SquareRoot
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var number = int.Parse(Console.ReadLine());
+                Console.WriteLine(Sqrt(number));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"Good bye");
+            }
+        }
+
+        private static double Sqrt(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"Number must be positive");
+            }
+
+            return Math.Pow(number, 0.5);
+        }
+    }
+}
