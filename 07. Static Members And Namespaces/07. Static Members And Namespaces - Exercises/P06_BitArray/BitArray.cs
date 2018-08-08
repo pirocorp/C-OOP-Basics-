f3ccc11592d2fb78dc5803f94a94b98f0fcb3ef7
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-08 10:14:47 +0200

    Problem 6. ** BitArray

---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P06_BitArray/BitArray.cs
@@ -0,0 +1,74 @@
+﻿namespace P06_BitArray
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Text;
+    
+    public class BitArray
+    {
+        private const int MaxBitsCount = 100000;
+        private const int MinBitsCount = 1;
+
+        private byte[] bitArray;
+
+        public BitArray(int size)
+        {
+            if (size >= MinBitsCount && size <= MaxBitsCount)
+            {
+                this.bitArray = new byte[size];
+            }
+            else
+            {
+                throw new ArgumentException($"Size of BitArray must be between 1 and 100000"); 
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (0 <= index && index < this.bitArray.Length)
+                {
+                    return this.bitArray[index];
+                }
+
+                throw new IndexOutOfRangeException($"Index {index} is invalid!");
+            }
+            set
+            {
+                if (index < 0 || index >= this.bitArray.Length)
+                {
+                    throw new IndexOutOfRangeException($"Index {index} is invalid!");
+                }
+
+                if (value < 0 || value > 1)
+                {
+                    throw new ArgumentException($"Value {value} is invalid!");
+                }
+
+                this.bitArray[index] = (byte)value;
+            }
+        }
+
+        private BigInteger ConvertToDecimal()
+        {
+            var result = new BigInteger();
+
+            for (var index = 0; index < this.bitArray.Length; index++)
+            {
+                if (this.bitArray[index] == 1)
+                {
+                    result += BigInteger.Pow(2, index);
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ConvertToDecimal()}";
+        }
+    }
+}
---
+++ 07. Static Members And Namespaces/07. Static Members And Namespaces - Exercises/P06_BitArray/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace P06_BitArray
+{
+    using System;
+    using System.Numerics;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var bitArray = new BitArray(100000);
+            Console.WriteLine(bitArray[99999]);
+            bitArray[99999] = 1;
+            Console.WriteLine(bitArray[99999]);
+            Console.WriteLine(bitArray);
+        }
+    }
+}
