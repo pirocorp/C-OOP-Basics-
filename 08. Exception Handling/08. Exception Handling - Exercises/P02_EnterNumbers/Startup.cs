107c4b7a7a58003c2a3a09c5f255a57800e1f4ba
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-08 20:33:00 +0200

    Problem 2. Enter Numbers

---
+++ 08. Exception Handling/08. Exception Handling - Exercises/P02_EnterNumbers/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace P02_EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var listOfNumbers = new List<int>();
+            
+            while (listOfNumbers.Count < 10)
+            {
+                try
+                {
+                    var number = int.Parse(Console.ReadLine());
+
+                    ReadNumber(1, 100, listOfNumbers, number);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            var numbers = listOfNumbers.OrderBy(x => x).ToArray();
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
+        }
+
+        private static void ReadNumber(int start, int end, List<int> listOfNumbers, int number)
+        {
+            if (number <= start || number >= end)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), $"Input number is out of range [{start}...{end}]");
+            }
+
+            listOfNumbers.Add(number);
+        }
+    }
+}
