01ad55cb81d0e5d143c8d556a42b60a67c95ea78
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-10 11:29:41 +0200

    Part II. Implementing LogInController Properties Helpers Constructor

--- 09. Workshop - Forum/Forum.App/Controllers/LoginController.cs
+++ 09. Workshop - Forum/Forum.App/Controllers/LoginController.cs
@@ -5,7 +5,31 @@
 
     public class LogInController : IController, IReadUserInfoController
     {
-        public string Username => throw new System.NotImplementedException();
+        public string Username { get; private set; }
+
+        private string Password { get; set; }
+
+        private bool Error { get; set; }
+
+        private enum Command
+        {
+            ReadUsername,
+            ReadPassword,
+            LogIn,
+            Back
+        }
+
+        private void ResetLogin()
+        {
+            this.Error = false;
+            this.Username = string.Empty;
+            this.Password = string.Empty;
+        }
+
+        public LogInController()
+        {
+            this.ResetLogin();
+        }
 
         public MenuState ExecuteCommand(int index)
         {
