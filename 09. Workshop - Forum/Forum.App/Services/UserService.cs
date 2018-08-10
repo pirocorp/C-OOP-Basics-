1086b1563da3fa509df760bac8f85a4a6710adbf
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-10 12:13:36 +0200

    Part IV. Implementing UserService Implement TryLogInUser

---
+++ 09. Workshop - Forum/Forum.App/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿namespace Forum.App.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Data;
+
+    public static class UserService
+    {
+        public static bool TryLoginUser(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            var forumData = new ForumData();
+
+            var userExists = forumData.Users.Any(x => x.Username == username && x.Password == password);
+
+            return userExists;
+        }
+
+        public static bool TrySignUpUser(string username, string password)
+        {
+
+        }
+    }
+}
