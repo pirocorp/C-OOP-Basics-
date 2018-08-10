2d6cebde262bba2ff71adb3dce0ce3a492cba562
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-10 12:01:39 +0200

    Part III. Implementing SingnUpController

--- 09. Workshop - Forum/Forum.App/Controllers/SignUpController.cs
+++ 09. Workshop - Forum/Forum.App/Controllers/SignUpController.cs
@@ -3,32 +3,77 @@
 	using Forum.App;
 	using Forum.App.Controllers.Contracts;
 	using Forum.App.UserInterface.Contracts;
+	using UserInterface;
 
-	public class SignUpController : IController, IReadUserInfoController
+    public class SignUpController : IController, IReadUserInfoController
 	{
 		private const string DETAILS_ERROR = "Invalid Username or Password!";
 		private const string USERNAME_TAKEN_ERROR = "Username already in use!";
 
-        public string Username => throw new System.NotImplementedException();
+	    public string Username { get; private set; }
+
+	    private string Password { get; set; }
+
+        private string ErrorMessage { get; set; }
+
+	    private enum Command
+	    {
+	        ReadUsername,
+	        ReadPassword,
+	        SignUp,
+	        Back
+	    }
+
+        public enum SignUpStatus
+        {
+            Success,
+            DetailsError,
+            UsernameTakenError
+        }
+
+	    private void ResetSignUp()
+	    {
+	        this.ErrorMessage = string.Empty;
+	        this.Username = string.Empty;
+	        this.Password = string.Empty;
+	    }
 
         public MenuState ExecuteCommand(int index)
         {
-            throw new System.NotImplementedException();
+            switch ((Command)index)
+            {
+                case Command.ReadUsername:
+                    this.ReadUsername();
+                    return MenuState.Signup;
+                case Command.ReadPassword:
+                    this.ReadPassword();
+                    return MenuState.Signup;
+                case Command.SignUp:
+                    //TODO Try signup
+                    return MenuState.Error;
+                case Command.Back:
+                    this.ResetSignUp();
+                    return MenuState.Back;
+            }
+
+            throw new System.InvalidOperationException();
         }
 
         public IView GetView(string userName)
         {
-            throw new System.NotImplementedException();
+            return new SignUpView(this.ErrorMessage);
         }
 
         public void ReadPassword()
         {
-            throw new System.NotImplementedException();
+            this.Password = ForumViewEngine.ReadRow();
+            ForumViewEngine.HideCursor();
         }
 
         public void ReadUsername()
         {
-            throw new System.NotImplementedException();
+            this.Username = ForumViewEngine.ReadRow();
+            ForumViewEngine.HideCursor();
         }
     }
 }
