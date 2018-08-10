0329ec6473797a13e07a6a989fc929f34b68d282
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-10 16:52:31 +0200

    Part VI. Implementing Add Post Controller Fields Helpers Properties

--- 09. Workshop - Forum/Forum.App/Controllers/AddPostController.cs
+++ 09. Workshop - Forum/Forum.App/Controllers/AddPostController.cs
@@ -2,6 +2,8 @@
 {
     using Forum.App.Controllers.Contracts;
     using Forum.App.UserInterface.Contracts;
+    using UserInterface.Input;
+    using UserInterface.ViewModels;
 
     public class AddPostController : IController
     {
@@ -9,6 +11,23 @@
         private const int TEXT_AREA_WIDTH = 37;
         private const int TEXT_AREA_HEIGHT = 18;
         private const int POST_MAX_LENGTH = 660;
+
+        private static int centerTop = Position.ConsoleCenter().Top;
+        private static int centerLeft = Position.ConsoleCenter().Left;
+
+        private enum Command
+        {
+            AddTitle,
+            AddCategory,
+            Write,
+            Post,
+        }
+
+        public PostViewModel Post { get; private set; }
+
+        private TextArea TextArea { get; set; }
+
+        public bool Error { get; private set; }
 
         public MenuState ExecuteCommand(int index)
         {
