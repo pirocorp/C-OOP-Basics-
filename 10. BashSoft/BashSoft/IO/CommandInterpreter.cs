730d7816e4866dc9849fb1e0397a586d393b9022
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-13 15:12:24 +0200

    Part IV. Polymorphism Problem 27. Creating the GetHelpCommand

--- 10. BashSoft/BashSoft/IO/CommandInterpreter.cs
+++ 10. BashSoft/BashSoft/IO/CommandInterpreter.cs
@@ -62,8 +62,7 @@
                     this.TryReadDatabaseFromFile(input, data);
                     break;
                 case "help":
-                    this.TryGetHelp(input, data);
-                    break;
+                    return new GetHelpCommand(input, data, this.judge, this.repository, this.inputOutputManager);
                 case "show":
                     this.TryShowWantedData(input, data);
                     break;
@@ -192,24 +191,6 @@
             {
                 this.DisplayInvalidCommandMessage(input);
             }
-        }
-
-        private void TryGetHelp(string input, string[] data)
-        {
-            OutputWriter.WriteMessageOnNewLine($"{new string('_', 100)}");
-            OutputWriter.WriteMessageOnNewLine($"|{"make directory - mkdir: path ",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"traverse directory - ls: depth ",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"comparing files - cmp: path1 path2",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"change directory - changeDirREl:relative path",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"change directory - changeDir:absolute path",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"read students data base - readDb: path",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"filter {courseName} excelent/average/poor  take 2/5/all students - filterExcelent",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"order increasing students - order {courseName} ascending/descending take 20/10/all",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"download file - download: path of file (saved in current directory)",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"download file asinchronously - downloadAsynch: path of file (save in the current directory)",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"|{"get help – help",-98}|");
-            OutputWriter.WriteMessageOnNewLine($"{new string('_', 100)}");
-            OutputWriter.WriteEmptyLine();
         }
 
         private void TryReadDatabaseFromFile(string input, string[] data)
