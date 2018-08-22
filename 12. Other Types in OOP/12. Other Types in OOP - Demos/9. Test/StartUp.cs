a0584d56f783694d74e9f61b6309aebda1d3de63
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-22 16:27:56 +0200

    12. Other Types in OOP

---
+++ 12. Other Types in OOP/12. Other Types in OOP - Demos/9. Test/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _9._Test
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var a = 5;
+            Modify(ref a);
+            Console.WriteLine(a);
+
+            var test = new Test(ref a);
+            test.Modify();
+            Console.WriteLine(a);
+        }
+
+        private static void Modify(ref int a)
+        {
+            a++;
+        }
+    }
+}
---
+++ 12. Other Types in OOP/12. Other Types in OOP - Demos/9. Test/Test.cs
@@ -0,0 +1,21 @@
+﻿namespace _9._Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Test
+    {
+        private dynamic a;
+
+        public Test(ref int a)
+        {
+            this.a = a;
+        }
+
+        public void Modify()
+        {
+            this.a++;
+        }
+    }
+}
