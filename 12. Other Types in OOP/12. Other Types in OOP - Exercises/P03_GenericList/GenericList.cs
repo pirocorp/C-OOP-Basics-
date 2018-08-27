d8ac2fa840ff443d95b8db0096ee4018bb826bdc
Zdravko Zdravkov <pirocorp@abv.bg>
2018-08-27 18:14:56 +0200

    Problem 3. Generic List

---
+++ 12. Other Types in OOP/12. Other Types in OOP - Exercises/P03_GenericList/GenericList.cs
@@ -0,0 +1,193 @@
+﻿namespace P03_GenericList
+{
+    using System;
+    using System.Linq;
+
+    public class GenericList<T> where T : IComparable<T>
+    {
+        private const int DEFAULTH_CAPAITY = 8;
+        private T[] elements;
+        private int currentIndex;
+
+        public GenericList(int initialCapacity = DEFAULTH_CAPAITY)
+        {
+            this.elements = new T[initialCapacity];
+            this.currentIndex = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (this.currentIndex >= this.elements.Length)
+            {
+                this.ResizeList();
+            }
+
+            this.elements[this.currentIndex] = element;
+            this.currentIndex++;
+        }
+
+        public int IndexOf(T element)
+        {
+            var result = -1;
+
+            for (var i = 0; i < this.currentIndex; i++)
+            {
+                var currentElement = this.elements[i];
+
+                if (currentElement.Equals(element))
+                {
+                    result = i;
+                    return result;
+                }
+            }
+
+            return result;
+        }
+
+        public bool Contains(T element)
+        {
+            var result = this.IndexOf(element);
+            return result > -1;
+        }
+
+        public void Remove(T element)
+        {
+            var index = this.IndexOf(element);
+
+            if (index == -1)
+            {
+                throw new ArgumentException("Specified element was not found.");
+            }
+
+            this.RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.CheckIndex(index);
+
+            for (var i = index; i < this.currentIndex - 1; i++)
+            {
+                var nextElement = this.elements[i + 1];
+
+                this.elements[i] = nextElement;
+            }
+
+            this.currentIndex--;
+            this.elements[this.currentIndex] = default(T);
+        }
+
+        public void InsertAt(T element, int index)
+        {
+            this.CheckIndex(index);
+
+            var previousElement = this.elements[index];
+            this.elements[index] = element;
+
+            if (this.currentIndex >= this.elements.Length)
+            {
+                this.ResizeList();
+            }
+
+            for (var i = index + 1; i <= this.currentIndex; i++)
+            {
+                var currentElement = this.elements[i];
+                this.elements[i] = previousElement;
+                previousElement = currentElement;
+            }
+
+            this.currentIndex++;
+        }
+
+        public T Max()
+        {
+            this.CheckIsEmpty();
+
+            T maxElement = this.elements[0];
+
+            for (var i = 1; i < this.currentIndex; i++)
+            {
+                if (maxElement.CompareTo(this.elements[i]) < 0)
+                {
+                    maxElement = this.elements[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        public T Min()
+        {
+            this.CheckIsEmpty();
+
+            T minElement = this.elements[0];
+
+            for (var i = 1; i < this.currentIndex; i++)
+            {
+                if (minElement.CompareTo(this.elements[i]) > 0)
+                {
+                    minElement = this.elements[i];
+                }
+            }
+
+            return minElement;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                this.CheckIndex(i);
+
+                return this.elements[i];
+            }
+            set
+            {
+                this.CheckIndex(i);
+
+                this.elements[i] = value;
+            }
+        }
+
+        public void Clear()
+        {
+            var length = this.elements.Length;
+            this.elements = new T[length];
+        }
+
+        public override string ToString()
+        {
+            return $"{string.Join(", ", this.elements.Take(this.currentIndex))}";
+        }
+
+        private void ResizeList()
+        {
+            var newElements = new T[this.elements.Length * 2];
+
+            for (var i = 0; i < this.elements.Length; i++)
+            {
+                var currentElement = this.elements[i];
+
+                newElements[i] = currentElement;
+            }
+
+            this.elements = newElements;
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (index < 0 || index >= this.currentIndex)
+            {
+                throw new IndexOutOfRangeException("Index was outside the boundaries of the CustomList");
+            }
+        }
+
+        private void CheckIsEmpty()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new ArgumentException("The list is empty");
+            }
+        }
+    }
+}
---
+++ 12. Other Types in OOP/12. Other Types in OOP - Exercises/P03_GenericList/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P03_GenericList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var list = new GenericList<int>();
+
+            list.Add(10);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Remove(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Remove(7);
+            Console.WriteLine(list);
+            Console.WriteLine(list.Max());
+            Console.WriteLine(list.Min());
+            Console.WriteLine(list[5]);
+            list.Add(12);
+            list.Add(13);
+            list.Add(14);
+            list.Add(15);
+            Console.WriteLine(list);
+            list.RemoveAt(0);
+            Console.WriteLine(list);
+            list.InsertAt(10, 0);
+            Console.WriteLine(list);
+        }
+    }
+}
