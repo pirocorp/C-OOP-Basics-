7c86d68f07ae97aec0d1ca9126530930a430b78b
Zdravko Zdravkov <pirocorp@abv.bg>
2018-09-28 17:56:04 +0200

    Exercises - Problem 2. Func

---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P02_Func/Extensions.cs
@@ -0,0 +1,27 @@
+﻿namespace P02_Func
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+            var result = new List<T>();
+
+            foreach (var element in collection)
+            {
+                if (condition(element))
+                {
+                    result.Add(element);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P02_Func/Functions.cs
@@ -0,0 +1,14 @@
+﻿namespace P02_Func
+{
+    using System;
+
+    public class Functions
+    {
+        static void Main()
+        {
+            int[] nums = {1, 2, 3, 6, 4, 11};
+            var result = nums.TakeWhile(x => x < 5);
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
