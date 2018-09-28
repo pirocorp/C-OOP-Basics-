80223524361e2e8b0ddbe8c1eb9255bb1983b8ff
Zdravko Zdravkov <pirocorp@abv.bg>
2018-09-28 17:40:00 +0200

    Exercise - P01_Predicates

---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P01_Predicates/Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_Predicates
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Extensions
+    {
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+            foreach (var element in collection)
+            {
+                if (condition(element))
+                {
+                    return element;
+                }
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P01_Predicates/Predicates.cs
@@ -0,0 +1,19 @@
+﻿namespace P01_Predicates
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Predicates
+    {
+        static void Main()
+        {
+            var elements = new List<int>()
+            {
+                1, 2, 3, 4, 6, 11, 3,
+            };
+
+            Console.WriteLine(elements.FirstOrDefault(x => x > 7));
+            Console.WriteLine(elements.FirstOrDefault(x => x < 0));
+        }
+    }
+}
