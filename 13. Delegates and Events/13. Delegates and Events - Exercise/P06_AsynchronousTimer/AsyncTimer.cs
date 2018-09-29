ac510dd2e6f968d953fe4c19135ff96ced3c01ae
Zdravko Zdravkov <pirocorp@abv.bg>
2018-09-29 14:10:23 +0200

    Exercise - Problem 3. Asynchronous Timer

---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P06_AsynchronousTimer/AsyncTimer.cs
@@ -0,0 +1,44 @@
+﻿namespace P06_AsynchronousTimer
+{
+    using System;
+    using System.Timers;
+
+    public class AsyncTimer
+    {
+        private Timer timer;
+
+        public AsyncTimer(Action action, int ticks, int timeInterval)
+        {
+            this.Action = action;
+            this.Ticks = ticks;
+            this.TimeInterval = timeInterval;
+            this.timer = new Timer(timeInterval);
+        }
+
+        public Action Action { get; }
+
+        public int Ticks { get; private set; }
+
+        public int TimeInterval { get; }
+
+        public void Run()
+        {
+            this.timer.Elapsed += this.OnTimeEvent;
+            this.timer.AutoReset = true;
+            this.timer.Enabled = true;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            this.Action();
+
+            if (this.Ticks <= 1)
+            {
+                this.timer.AutoReset = false;
+                this.timer.Enabled = false;
+            }
+
+            this.Ticks--;
+        }
+    }
+}
---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P06_AsynchronousTimer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace P06_AsynchronousTimer
+{
+    using System;
+
+    public class StartUp
+    {
+        private static int _tick = 1;
+
+        public static void Main()
+        {
+            var timer = new AsyncTimer(PrintToConsole, 100, 1000);
+            timer.Run();
+            var input = Console.ReadLine();
+        }
+
+        public static void PrintToConsole()
+        {
+            Console.WriteLine($"{_tick++}");
+        }
+    }
+}
