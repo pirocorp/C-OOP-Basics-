d9040fbe7624a9165aa74406ca5d78e33cfdaf35
Zdravko Zdravkov <pirocorp@abv.bg>
2018-09-29 13:12:47 +0200

    Exercise - Problem 2. Interest Calculator

---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P05_InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace P05_InterestCalculator
+{
+    using System;
+
+    public class InterestCalculator
+    {
+        public InterestCalculator(decimal intitialDeposit, decimal interest, int years, Func<decimal, decimal, int, decimal> calculateInterest)
+        {
+            this.IntitialDeposit = intitialDeposit;
+            this.Interest = interest;
+            this.Years = years;
+            this.CalculateInterest = calculateInterest;
+        }
+
+        public decimal IntitialDeposit { get; }
+
+        public decimal Interest { get; }
+
+        public int Years { get; }
+
+        public Func<decimal, decimal, int, decimal> CalculateInterest { get; }
+
+        public decimal TotalPayout => this.CalculateInterest(this.IntitialDeposit, this.Interest, this.Years);
+    }
+}
---
+++ 13. Delegates and Events/13. Delegates and Events - Exercise/P05_InterestCalculator/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace P05_InterestCalculator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var compoundInterestCalculator = new InterestCalculator(500M, 0.056M, 10, GetCompoundInterest);
+            var simpleInterestCalculator = new InterestCalculator(2500M, 0.072M, 15, GetSimpleInterest);
+            Console.WriteLine($"{compoundInterestCalculator.TotalPayout:F4}");
+            Console.WriteLine($"{simpleInterestCalculator.TotalPayout:F4}");
+        }
+
+        private static decimal GetCompoundInterest(decimal sumOfMoney, decimal interest, int years)
+        {
+            var numberOfCompoundsPerYear = 12;
+            var arg1 = (double)(1 + interest / numberOfCompoundsPerYear);
+            var arg2 = numberOfCompoundsPerYear * years;
+            var compoundInterest = Math.Pow(arg1, arg2);
+            return sumOfMoney * (decimal)compoundInterest;
+        }
+
+        private static decimal GetSimpleInterest(decimal sumOfMoney, decimal interest, int years)
+        {
+            return sumOfMoney * (1 + interest * years);
+        }
+    }
+}
