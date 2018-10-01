4dcdd54d8db3822677611248cd7e0b2f7399c155
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-01 16:30:07 +0200

    Exercise - Problem 1. Country

---
+++ 14. Common Type System/14. Common Type System - Exercise/P01_Country/Country.cs
@@ -0,0 +1,175 @@
+﻿namespace P01_Country
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Country : IComparable<Country>, ICloneable
+    {
+        private string name;
+        private ulong population;
+        private double area;
+        private HashSet<string> cities;
+
+        public Country(string name, ulong population, double area, params string[] inputCities)
+        {
+            this.Name = name;
+            this.Population = population;
+            this.Area = area;
+            this.cities = new HashSet<string>(inputCities);
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Country name cannot be empty!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public ulong Population
+        {
+            get => this.population;
+            private set => this.population = value;
+        }
+
+        public double Area
+        {
+            get => this.area;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"Country area cannot be negative number!");
+                }
+
+                this.area = value;
+            }
+        }
+
+        public IReadOnlyCollection<string> Cities => this.cities;
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Population: {this.Population}, Area: {this.Area}, Cities: [{string.Join(", ", this.Cities)}]";
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public int CompareTo(Country other)
+        {
+            var areaComparator = SignOfNumber(this.Area - other.Area);
+            var populationComparator = SignOfNumber(this.Population - other.Population);
+            var nameComparator = this.Name.CompareTo(other.Name);
+
+            if (areaComparator != 0)
+            {
+                return -1 * areaComparator;
+            }
+
+            if (populationComparator != 0)
+            {
+                return -1 * populationComparator;
+            }
+
+            return nameComparator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Country;
+
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (string.Equals(this.Name.ToLower(), other.Name.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator ==(Country firstCountry, Country secondCountry)
+        {
+            if (ReferenceEquals(firstCountry, null) || ReferenceEquals(secondCountry, null))
+            {
+                return false;
+            }
+
+            return firstCountry.Equals(secondCountry);
+        }
+
+        public static bool operator !=(Country firstCountry, Country secondCountry)
+        {
+            if (ReferenceEquals(firstCountry, null) || ReferenceEquals(secondCountry, null))
+            {
+                return false;
+            }
+
+            return !firstCountry.Equals(secondCountry);
+        }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        public Country Clone()
+        {
+            var clone = new Country(this.Name, this.Population, this.Area);
+
+            foreach (var city in this.Cities)
+            {
+                clone.cities.Add(city);
+            }
+
+            return clone;
+        }
+
+        public void AddCity(string city)
+        {
+            this.cities.Add(city);
+        }
+
+        private static int SignOfNumber(double number)
+        {
+            if (number > 0)
+            {
+                return 1;
+            }
+
+            if (number < 0)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private static int SignOfNumber(ulong number)
+        {
+            if (number > 0)
+            {
+                return 1;
+            }
+
+            if (number < 0)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 14. Common Type System/14. Common Type System - Exercise/P01_Country/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace P01_Country
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var countries = new List<Country>()
+            {
+                new Country("Bulgaria", 7100000, 111000, "Sofia", "Plovdiv", "Varna"),
+                new Country("USA", 300000000, 1200000, "New York", "Los Angeles", "San Francisco"),
+                new Country("Bulgaria", 8000000, 10),
+                new Country("Bulgaria", 8000000, 111000),
+                new Country("Croatia", 8000000, 111000)
+            };
+
+            countries.Sort();
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+            }
+
+            Country bg = new Country("Bulgaria", 7100000, 111000, "Sofia", "Plovdiv", "Varna");
+            Country usa = new Country("USA", 300000000, 1200000, "New York", "Los Angeles", "San Francisco");
+            Country bg2 = new Country("Bulgaria", 8000000, 10);
+            Country bg3 = new Country("Bulgaria", 8000000, 111000);
+            Country hr = new Country("Croatia", 8000000, 111000);
+
+            Console.WriteLine();
+            Console.WriteLine(bg == bg2); // True
+            Console.WriteLine(bg == usa); // False
+            Console.WriteLine(bg != bg2); // False
+            Console.WriteLine(bg != usa); // True
+
+            var bgCopy = bg.Clone() as Country;
+            bg.AddCity("Kaspichan");
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", bg.Cities));
+            Console.WriteLine(string.Join(", ", bgCopy.Cities));
+        }
+    }
+}
