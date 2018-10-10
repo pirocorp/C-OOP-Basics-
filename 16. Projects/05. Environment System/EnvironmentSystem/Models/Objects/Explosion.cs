680ed9ca91116652a5214ade50736f96208d20cd
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-10 17:54:42 +0200

    Problem 6.	Unstable Stars Step 1 – Instability

---
+++ 16. Projects/05. Environment System/EnvironmentSystem/Models/Objects/Explosion.cs
@@ -0,0 +1,23 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DataStructures;
+
+    public class Explosion : Tail
+    {
+        public Explosion(int x, int y, int lifetime = 2) 
+            : base(x, y, lifetime)
+        {
+            this.CollisionGroup = CollisionGroup.Explosion;
+        }
+
+        public Explosion(Rectangle bounds) 
+            : base(bounds)
+        {
+        }
+    }
+}
---
+++ 16. Projects/05. Environment System/EnvironmentSystem/Models/Objects/UnstableStar.cs
@@ -0,0 +1,54 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    public class UnstableStar : FallingStar
+    {
+        private int lifetime;
+
+        public UnstableStar(int x, int y, Point direction, int lifetime = 8) 
+            : base(x, y, direction)
+        {
+            this.lifetime = lifetime;
+        }
+
+        public override void Update()
+        {
+            this.lifetime--;
+
+            if (this.lifetime <= 0)
+            {
+                this.Exists = false;
+            }
+
+            base.Update();
+        }
+
+        public override IEnumerable<EnvironmentObject> ProduceObjects()
+        {
+            if (this.Exists)
+            {
+                return base.ProduceObjects();
+            }
+            else
+            {
+                var explosions = new List<EnvironmentObject>();
+
+                for (var y = this.Bounds.TopLeft.Y - 2; y <= this.Bounds.TopLeft.Y + 2; y++)
+                {
+                    for (var x = this.Bounds.TopLeft.X - 2; x <= this.Bounds.TopLeft.X + 2; x++)
+                    {
+                        if (x != this.Bounds.TopLeft.X || y != this.Bounds.TopLeft.Y)
+                        {
+                            var currentExplosion = new Explosion(x, y);
+                            explosions.Add(currentExplosion);
+                        }
+                    }
+                }
+
+                return explosions;
+            }
+        }
+    }
+}
